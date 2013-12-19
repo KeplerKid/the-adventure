bf6922787f3437edb10ef2ec3f45da6a5fbaa8c1
unknown <A3MYDZZ@W0144254.usac.mmm.com>
2013-12-19 09:23:31 -0600

    Comment on Condition

--- src/com/bigeauofn/adventure/models/Condition.java
+++ src/com/bigeauofn/adventure/models/Condition.java
@@ -1,5 +1,12 @@
 package com.bigeauofn.adventure.models;
 
+/**
+ * This class represents a status and a requirement for what that status needs
+ * to be in order for the condition to be met.
+ * 
+ * @author JimseyJoe
+ * 
+ */
 public class Condition {
 
 }
