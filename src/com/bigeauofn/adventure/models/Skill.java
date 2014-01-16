965a58319ca084c7bb1349968ed63ec93ed9a6b2
Nik <nlettner@gmail.com>
2014-01-15 21:37:39 -0600

    Utilised StringUtility to format Skill.toString() and Skills.toString().

--- src/com/bigeauofn/adventure/models/Skill.java
+++ src/com/bigeauofn/adventure/models/Skill.java
@@ -1,82 +1,112 @@
 package com.bigeauofn.adventure.models;
 
 import com.bigeauofn.adventure.dicebag.RollResult;
+import com.bigeauofn.adventure.utilities.StringUtility;
 
 public class Skill implements D20C {
-	private String name;
-	private String type;
-	private Actor actor;
-	private String abilityName;
-	private int armourPenalty;
-	private int misc;
-	private Boolean trained = false;
-	
-	public Skill(String name, Actor a,String abilityName,  String type){
-		this.name = name;
-		this.actor = a;
-		this.type = type;
-		this.abilityName = abilityName;
-	}
-	
-	
-	public String getName(){
-		return this.name;
-	}
-	public int getValue(){
-		int toReturn = 0;
-		System.out.println(abilityName+ "\n\n\n");
-		toReturn += actor.getAbilityScore(abilityName).getValue();
-		toReturn += actor.getNotDoneYet().get("level").getValue() / 2;
-		toReturn -= this.armourPenalty;
-		if(this.trained){
-			toReturn += 5;
-		}
-		toReturn += this.misc;
-		
-		return toReturn;
-	}
-	public String getType(){
-		return this.type;
-	}
-	public void setArmourPenalty(int penalty){
-		this.armourPenalty = penalty;
-	}
-	public void setTrained(){
-		this.trained = true;
-	}
-	public void setMisc(int misc){
-		this.misc = misc;
-	}
-	
+    private String name;
+    private String type;
+    private Actor actor;
+    private String abilityName;
+    private int armourPenalty;
+    private int misc;
+    private Boolean trained = false;
 
-	
-	public int compareToRollResult(RollResult rr){
-		// TODO
-		
-		return 4;
-	}
-	
-	@Override
-	public String toString(){
-		return "Skill Named - " + this.name + ", Has Value = " + this.getValue() + ", Trained = " + this.trained.toString();
-	}
+    public Skill(String name, Actor a, String abilityName, String type) {
+        this.name = name;
+        this.actor = a;
+        this.type = type;
+        this.abilityName = abilityName;
+    }
 
 
-	@Override
-	public String getCompareType() {
-		return this.type;
-	}
+    public String getName() {
+        return this.name;
+    }
+
+    public int getValue() {
+        int toReturn = 0;
+        toReturn += actor.getAbilityScore(abilityName).getValue();
+        toReturn += actor.getNotDoneYet().get("level").getValue() / 2;
+        toReturn -= this.armourPenalty;
+        if (this.trained) {
+            toReturn += 5;
+        }
+        toReturn += this.misc;
+
+        return toReturn;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setArmourPenalty(int penalty) {
+        this.armourPenalty = penalty;
+    }
+
+    public void setTrained() {
+        this.trained = true;
+    }
+
+    public void setMisc(int misc) {
+        this.misc = misc;
+    }
 
 
-	@Override
-	public String getCompareKey() {
-		return this.name;
-	}
+    public int compareToRollResult(RollResult rr) {
+        // TODO
+
+        return 4;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.name);
+        sb.append(" : ");
+        sb.append(this.getValue());
+        sb.append(" | Trained : ");
+        sb.append(this.trained);
+        return sb.toString();
+    }
+
+    public String toString(int maxName, int maxValue) {
+        StringBuilder sb = new StringBuilder();
+        String value;
+        int valueLength;
+        sb.append(this.name);
+        if (this.name.length() < maxName) {
+            sb.append(StringUtility.spaces(maxName - this.name.length()));
+        }
+        sb.append(" : ");
+        value = Integer.toString(this.getValue());
+        valueLength = value.length();
+        if (valueLength < maxValue) {
+            sb.append(StringUtility.spaces(maxValue - valueLength));
+        }
+        sb.append(value);
+        sb.append(" | Trained : ");
+        sb.append(this.trained);
+        return sb.toString();
+    }
 
 
-	@Override
-	public String getCompareValue() {
-		return this.getValue() + "";
-	}
+    @Override
+    public String getCompareType() {
+        return this.type;
+    }
+
+
+    @Override
+    public String getCompareKey() {
+        return this.name;
+    }
+
+
+    @Override
+    public String getCompareValue() {
+        return this.getValue() + "";
+    }
 
 }
--- src/com/bigeauofn/adventure/models/Skills.java
+++ src/com/bigeauofn/adventure/models/Skills.java
@@ -1,6 +1,7 @@
 package com.bigeauofn.adventure.models;
 
 import com.bigeauofn.adventure.utilities.ListUtility;
+import com.bigeauofn.adventure.utilities.StringUtility;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -9,93 +10,93 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Skills {
-	private HashMap<String, Skill> skillList;
-	private Actor actor;
-	
-	public Skills(Actor actor){
-		this.actor = actor;
+    private HashMap<String, Skill> skillList;
+    private Actor actor;
+
+    public Skills(Actor actor) {
+        this.actor = actor;
         skillList = new HashMap<>();
         this.loadBaseSkills();
     }
-	
-	public Skill getSkill(String skillName) {
-		return skillList.get(skillName);
-	}
-	public void addTrainedSkills(HashMap<String, Boolean> tempSolution){
-		for(String s : tempSolution.keySet()){
-			if(tempSolution.get(s)){
-				this.getSkill(s).setTrained();
-			}
-		}
-	}
-	
-	@Override
-	public String toString(){
-		StringBuilder toReturn = new StringBuilder();
-		for(String s : ListUtility.asSortedList(this.skillList.keySet())){
-            toReturn.append(this.skillList.get(s).toString());
+
+    public Skill getSkill(String skillName) {
+        return skillList.get(skillName);
+    }
+
+    public void addTrainedSkills(HashMap<String, Boolean> tempSolution) {
+        for (String s : tempSolution.keySet()) {
+            if (tempSolution.get(s)) {
+                this.getSkill(s).setTrained();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder toReturn = new StringBuilder();
+        List<String> keyList = ListUtility.asSortedList(this.skillList.keySet());
+        int maxSkillNameLength = StringUtility.maxStringLength(keyList);
+        for (String s : keyList) {
+            toReturn.append(this.skillList.get(s).toString(maxSkillNameLength, 2));
             toReturn.append('\n');
         }
-		
-		return toReturn.toString();
-	}
-	
 
-	
-	
-	private void loadBaseSkills(){
-		String path = "actors/Skill_List.txt";
+        return toReturn.toString();
+    }
 
-		BufferedReader br = openFileForReading(path);
 
-		try {
-			String line;
-			while ((line = br.readLine()) != null) {
+    private void loadBaseSkills() {
+        String path = "actors/Skill_List.txt";
 
-				if (!line.startsWith(";") && line.contains("=")) {
+        BufferedReader br = openFileForReading(path);
 
-						// add a new KV pair
-						int splitter = line.indexOf("=");
-						String abilityName = line.substring(1, splitter-1);
-						String values = line.substring(splitter + 1,line.length());
-						ArrayList<String> namesOfSkills;
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                if (!line.startsWith(";") && line.contains("=")) {
+
+                    // add a new KV pair
+                    int splitter = line.indexOf("=");
+                    String abilityName = line.substring(1, splitter - 1);
+                    String values = line.substring(splitter + 1, line.length());
+                    ArrayList<String> namesOfSkills;
                     namesOfSkills = new ArrayList<>(Arrays.asList(values.split(",")));
                     for (String s : namesOfSkills) {
-                        Skill toPut = new Skill(s,this.actor,abilityName,"skill");
-							this.skillList.put(s, toPut);
-						}
+                        Skill toPut = new Skill(s, this.actor, abilityName, "skill");
+                        this.skillList.put(s, toPut);
+                    }
 
-					} else {
+                } else {
                     continue;
                     // do nothing, ; indicates comment
                 }
-				}
-			
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private BufferedReader openFileForReading(String fileNameAndPath) {
-		// create the file reader
-		FileReader fr = null;
-		try {
-			fr = new FileReader(fileNameAndPath);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return new BufferedReader(fr);
-	}
-
-	public boolean isNewThing(String line) {
-		return line.startsWith("[");
-	}
+            }
 
 
-	
-	
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private BufferedReader openFileForReading(String fileNameAndPath) {
+        // create the file reader
+        FileReader fr = null;
+        try {
+            fr = new FileReader(fileNameAndPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new BufferedReader(fr);
+    }
+
+    public boolean isNewThing(String line) {
+        return line.startsWith("[");
+    }
+
+
 }
