ddfa57e46deb4424352e69f7a30d351d4ded6e8c
Nik <nlettner@gmail.com>
2014-01-15 21:44:04 -0600

    Fixed AbilityScore.getMod() to return the correct values. Added a <sigh> lengthy comment about comparing the double/Math.floor() based implementation against a pure integer version that doesn't work. This is not to dissuade anyone from trying to find a pure integer one. It's possible one exists.

--- src/com/bigeauofn/adventure/models/AbilityScore.java
+++ src/com/bigeauofn/adventure/models/AbilityScore.java
@@ -2,6 +2,8 @@ package com.bigeauofn.adventure.models;
 
 import com.bigeauofn.adventure.dicebag.RollResult;
 
+import java.io.PrintStream;
+
 public class AbilityScore implements D20C {
 	private String name;
 	private String type;
@@ -23,15 +25,70 @@ public class AbilityScore implements D20C {
 	public String getType(){
 		return this.type;
 	}
-	public int getMod(){
-		return Math.max((this.value -10) - 5,0);
-	}
 
-	
-	
+    public static void printModTable(int minAbilityScore, int maxAbilityScore, PrintStream out) {
+        StringBuilder sb;
+        int intDivResult, doubleDivResult;
 
-	
-	public int compareToRollResult(RollResult rr){
+        for (int i = minAbilityScore; i <= maxAbilityScore; i++) {
+            sb = new StringBuilder();
+            sb.append("AbilityScore : ");
+            sb.append(i);
+            sb.append("\t(i - 10) / 2 : ");
+            intDivResult = ((i - 10) / 2);
+            sb.append(intDivResult);
+            sb.append("\t(int)Math.floor((i - 10) / 2.0) : ");
+            doubleDivResult = (int) Math.floor((i - 10) / 2.0);
+            sb.append(doubleDivResult);
+            if (doubleDivResult >= 0) {
+                sb.append("\t");
+            }
+            sb.append("\tint modifier calulation correct? : ");
+            sb.append(intDivResult == doubleDivResult);
+            out.println(sb.toString());
+        }
+    }
+
+    public static void printModTable(int minAbilityScore, int maxAbilityScore) {
+        printModTable(minAbilityScore, maxAbilityScore, System.out);
+    }
+
+    public int getMod() {
+        return (int) Math.floor((this.value - 10) / 2.0);
+        /*
+         * This :
+         *  return (int)((this.value - 10) / 2);
+         * is incorrect for this.value == [1, 9]
+         *
+         * IF YOU'RE CURIOUS
+         * ([1, 9] - 10) / 2 = [-9, -1] / 2
+         *
+         * So how java and most processors
+         *    implement integer division is your issue
+         * [-9, -1] / 2
+         *
+         * if it were to implement it proper Euclidean Division
+         *  a / b = q,r for q,r such that a = b * q + r where 0 <= r < |b|
+         *     so -9 / 2 = -5,1 (since -9 = 2 * -5 + 1)
+         *      and -1 / 2 = -1,1 (since -1 = 2 * -1 + 1)
+         *       and so on ...
+         *
+         * This usually isn't implemented with two's complement hardware
+         *  (which is good for adding and subtracting integers)
+         * instead it's implemented like this
+         *  a / b = q:r for q,r such that a = b * q + r where 0 <= |r| < b
+         *    so -9 / 2 = -4,-1 (since -9 = 2 * -4 + (-1))
+         *     and -1 / 2 = 0,-1 (since -1 = 2 * 0 + (-1))
+         *      and so on ...
+         *  This unfortunately yields the incorrect mod values for ability scores [1, 9]
+         *
+         * This however confuses me greatly:
+		 * return Math.max((this.value -10) - 5,0);
+		 */
+    }
+
+
+    public int compareToRollResult(RollResult rr){
 		// TODO
 		
 		return 4;
